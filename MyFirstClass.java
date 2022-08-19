f49dd6ddc2c28648b28f93bcd471ea11cd79f989
kristian0528 <krtr3302@student.harmonytx.org>
2022-08-19 08:51:00 -0500

    This is my first commit

---
+++ MyFirstClass.java
@@ -0,0 +1,21 @@
+public class MyFirstClass
+{
+    //after you create project and class
+    //you have to create the main method
+    //in order to use commands type main and tab
+    public static void main(String[] args) {
+        //type sout then tab and add quotes for lines
+        //each line ends with a semicolon
+        //double dash makes the code a comment
+        /*
+        This is how you create a multiple line comment
+        in your code and can type as much as you want
+         */
+        System.out.println("My First Line of Code");
+        /*
+        println prints out your sentence in the next line
+        print prints the text in the same line until you change it
+         */
+
+    }
+}
