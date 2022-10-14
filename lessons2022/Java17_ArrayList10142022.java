7be664fdda0df5a28383ecdfd99989f2701dbf98
kristian0528 <krtr3302@student.harmonytx.org>
2022-10-14 08:55:12 -0500

    Today is 10/13/2022

---
+++ lessons2022/Java17_ArrayList10142022.java
@@ -0,0 +1,76 @@
+package lessons2022;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Java17_ArrayList10142022 {
+    public static void main(String[] args) {
+        //Array list
+        //presented in java util class
+        //is an ordered collection of values
+        //ArrayList is dynamic size arrays, do not have to specify
+        //Can add and remove arrays from ArrayList
+        //Only stores objects
+
+        //Decleration
+        ArrayList<String> arrList = new ArrayList<>();
+        //or
+        ArrayList<String> arrList2 = new ArrayList<String>();
+        //or
+        ArrayList<Integer> list10 = new ArrayList<>(Arrays.asList(1,0,2,0,3,0,4,0));
+
+        //Adding array
+        arrList.add("Kristian");
+        arrList.add("Alejandro");
+        arrList.add("Nathaniel");
+        System.out.println(arrList);
+
+        //Get
+        System.out.println(arrList.get(1));
+
+        //Size
+        System.out.println(arrList.size());
+
+        //Clear removes all values
+        arrList.clear();
+        System.out.println(arrList);
+
+        //Set
+        arrList.add(0,"Trent");
+        System.out.println(arrList);
+        arrList.set(0,"Kristian");
+        System.out.println(arrList);
+
+        //Contains
+        System.out.println(arrList.contains("Trent"));
+        System.out.println(arrList.contains("Kristian"));
+
+        //equals method - check if two arrays are the same
+        ArrayList<String> arr1 = new ArrayList<>(Arrays.asList("a","b","c"));
+        ArrayList<String> arr2 = new ArrayList<>(Arrays.asList("a","b","c"));
+        System.out.println(arr1.equals(arr2));// this will be true
+        System.out.println(arrList.equals(arr2)); //this will be false
+
+        //Remove method
+        System.out.println(arr1);
+        arr1.remove("b");
+        System.out.println(arr1);
+
+        //Index of method
+        System.out.println(arr2.indexOf("c"));
+        System.out.println(arr1.indexOf("c"));
+
+        //Last Index of
+        arr2.add("4");
+        arr2.add("5");
+        arr2.add("6");
+        arr2.add("a");
+        System.out.println(arr2.lastIndexOf("a"));
+        System.out.println(arr2.indexOf("a"));
+
+        //Empty checks if the list is empty
+        System.out.println(arr2.isEmpty());
+        arr2.clear();
+        System.out.println(arr2.isEmpty());
+    }
+}
